93ff7023830bf530bfe015cf7bf16a2e050dd016
acornmonkey <111863988+acornmonkey@users.noreply.github.com>
2025-04-20 15:51:36 +1000

    names are now appropriately shortened if they have multiple words

--- rangeMap.js
+++ rangeMap.js
@@ -1,13 +1,25 @@
 async function rangeMap(parent,extra) {
 	const parentInfo=await (await fetch('https://api.inaturalist.org/v1/taxa/'+parent)).json();
 	const children=parentInfo.results[0].children;
-	let sObj={queries:[],tab:"map",taxonFrequenciesSortOrder:"asc",colorScheme:"categorical"};
+	const sObj={queries:[],tab:"map",taxonFrequenciesSortOrder:"asc",colorScheme:"categorical"};
 	if (extra==null) {
 		extra='';
 	}
 	for (let i=0;i<children.length;i++) {
 		sObj.queries.push({});
-		//if (children[i].name.includes("×")) {
+		let nameArray=children[i].name.split(' ');
+		let wordCount=nameArray.length;
+		if ( !children[i].name.includes('×') || (children[i].name[0]==='×' && wordCount===3) ) {
+			children[i].name=nameArray[wordCount-1];
+		} else if (wordCount===5 && nameArray[3]==='×') {
+			children[i]=nameArray[2] + ' × ' + nameArray[4];
+		} else if (wordCount>==3 && wordCount<==6 && wordCount!==5) {
+			children[i]=nameArray[1] + ' ' + nameArray[2];
+			for (let j=3;j<wordCount;j++) {
+				children[i]+=' ';
+				children[i]+=nameArray[j];
+			}
+		}
 		sObj.queries[i].name=String.fromCharCode.apply(null,new TextEncoder().encode(children[i].name));
 		sObj.queries[i].params='taxon_id='+children[i].id+extra;
 	}
